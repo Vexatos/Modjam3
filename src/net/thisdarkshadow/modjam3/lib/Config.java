07f4efde449b36d1de35ab8156f73f1bdcae3c98
Tyler Hancock <darklime@live.ca>
2013-12-13 08:43:33 -0700

    Event changes.

--- src/net/thisdarkshadow/modjam3/lib/Config.java
+++ src/net/thisdarkshadow/modjam3/lib/Config.java
@@ -8,14 +8,20 @@ public class Config {
 
 	public static Configuration conf;
 	
+	public static boolean playerMessage;
+	
+	public static int crystalID;
+	
 	public Config(File file) {
 		
 		conf = new Configuration(file);
+		
+		conf.load();
+		
+		playerMessage = conf.get(conf.CATEGORY_GENERAL, "Should messages sent to the player also be sent to the server/console", false).getBoolean(false);
+		
+		crystalID = conf.get(conf.CATEGORY_ITEM, "Item ID for Summoning Crystal", 24380).getInt();
+		
+		conf.save();
 	}
-	
-	//General
-	public static boolean playerMessage = conf.get(conf.CATEGORY_GENERAL, "Should messages sent to the player also be sent to the server/consol", false).getBoolean(false);
-	
-	//Item ID
-	public static int crystalID = conf.get(conf.CATEGORY_ITEM, "Item ID for Summoning Crystal", 24380).getInt();
 }
