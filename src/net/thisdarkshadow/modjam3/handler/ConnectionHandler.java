7f91e187264c4727923cea54213d5a4bf338ec48
ShadowChild <shadychild12@gmail.com>
2013-12-13 17:07:53 +0000

    you dont need to create and instance
    Signed-off-by: ShadowChild <shadychild12@gmail.com>

--- src/net/thisdarkshadow/modjam3/handler/ConnectionHandler.java
+++ src/net/thisdarkshadow/modjam3/handler/ConnectionHandler.java
@@ -1,16 +1,15 @@
 package net.thisdarkshadow.modjam3.handler;
 
+import cpw.mods.fml.common.network.IConnectionHandler;
+import cpw.mods.fml.common.network.Player;
 import net.minecraft.network.INetworkManager;
 import net.minecraft.network.NetLoginHandler;
 import net.minecraft.network.packet.NetHandler;
 import net.minecraft.network.packet.Packet1Login;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatMessageComponent;
-import net.thisdarkshadow.modjam3.ModJam;
 import net.thisdarkshadow.modjam3.lib.Config;
 import net.thisdarkshadow.modjam3.lib.Format;
-import cpw.mods.fml.common.network.IConnectionHandler;
-import cpw.mods.fml.common.network.Player;
 
 public class ConnectionHandler implements IConnectionHandler {
 
@@ -25,10 +24,10 @@ public class ConnectionHandler implements IConnectionHandler {
 			String message2 = " esper.net and send a special command to the user named Umbreon you will be added to the list of";
 			String message3 = " users who will get a special cape. This event is only around for the duration of modjam!";
 			String message4 = "The command to send is !activate-modjam3-yourInGameName If you have any issues please contact Darkhax or ShadowChild";
-			netHandler.getPlayer().sendChatToPlayer(new ChatMessageComponent().createFromText(Format.DARK_YELLOW + message1 + message2 + message3));
-			netHandler.getPlayer().sendChatToPlayer(new ChatMessageComponent().createFromText("          "));
-			netHandler.getPlayer().sendChatToPlayer(new ChatMessageComponent().createFromText(Format.DARK_YELLOW + message4));
-			netHandler.getPlayer().sendChatToPlayer(new ChatMessageComponent().createFromText(Format.DARK_YELLOW + "This message can be disabled in the configs."));
+			netHandler.getPlayer().sendChatToPlayer(ChatMessageComponent.createFromText(Format.DARK_YELLOW + message1 + message2 + message3));
+			netHandler.getPlayer().sendChatToPlayer(ChatMessageComponent.createFromText("          "));
+			netHandler.getPlayer().sendChatToPlayer(ChatMessageComponent.createFromText(Format.DARK_YELLOW + message4));
+			netHandler.getPlayer().sendChatToPlayer(ChatMessageComponent.createFromText(Format.DARK_YELLOW + "This message can be disabled in the configs."));
 		}
 	}
 
