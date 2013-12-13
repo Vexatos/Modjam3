cda5ebc058cbf9ac5212eb5aa33b436a29b29a5e
Tyler Hancock <darklime@live.ca>
2013-12-12 23:45:44 -0700

    Automated Modjam Commit: 23:45:44.21

--- src/net/thisdarkshadow/modjam3/item/ItemSpawningCrystal.java
+++ src/net/thisdarkshadow/modjam3/item/ItemSpawningCrystal.java
@@ -6,14 +6,10 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
 
 public class ItemSpawningCrystal extends Item {
-
-	private String author;
-	private String type;
-	private String name;
-	private int expereince;
-	private int level;
 	
 	public ItemSpawningCrystal(int id) {
 		
@@ -24,10 +20,79 @@ public class ItemSpawningCrystal extends Item {
 		this.setUnlocalizedName("modjam3.spawnCrystal");
 	}
 	
+	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+		
+		if (!stack.hasTagCompound()) {
+		
+			stack.setTagCompound(new NBTTagCompound());
+		}
+		
+		if (!stack.stackTagCompound.hasKey("owner")) {
+			
+			stack.stackTagCompound.setString("owner", player.username);
+			stack.stackTagCompound.setString("type", player.username);
+			stack.stackTagCompound.setString("name", player.username);
+			stack.stackTagCompound.setInteger("level", 1);
+			stack.stackTagCompound.setInteger("experience", 0);
+		}
+		
+		return stack;	
+	}
 	
+	public static String getOwner(ItemStack stack) {
+		
+		if (stack.getItem() instanceof ItemSpawningCrystal) {
+			
+			return stack.stackTagCompound.getString("owner");
+		}
+		
+		return "Failure";
+	}
+	
+	public static String getType(ItemStack stack) {
+		
+		if (stack.getItem() instanceof ItemSpawningCrystal) {
+			
+			return stack.stackTagCompound.getString("type");
+		}
+		
+		return "Failure";
+	}
+	
+	public static String getName(ItemStack stack) {
+		
+		if (stack.getItem() instanceof ItemSpawningCrystal) {
+			
+			return stack.stackTagCompound.getString("name");
+		}
+		
+		return "Failure";
+	}
+	
+	public static int getLevel(ItemStack stack) {
+		
+		if (stack.getItem() instanceof ItemSpawningCrystal) {
+			
+			return stack.stackTagCompound.getInteger("level");
+		}
+		
+		return 0;
+	}
+	
+	public static int getExperience(ItemStack stack) {
+		
+		if (stack.getItem() instanceof ItemSpawningCrystal) {
+			
+			return stack.stackTagCompound.getInteger("experience");
+		}
+		
+		return 9999999;
+	}
 	
 	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean smt) {
 		
-		list.add("Test");
+		if (stack.stackTagCompound.hasKey("owner")) {
+			list.add("");
+		}
 	}
 }
