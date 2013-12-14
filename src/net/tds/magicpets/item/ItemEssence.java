83ed3c90109baae5ecb50344fe2ef9e034179135
Tyler Hancock <darklime@live.ca>
2013-12-14 12:54:52 -0700

    Added shiny

--- src/net/tds/magicpets/item/ItemEssence.java
+++ src/net/tds/magicpets/item/ItemEssence.java
@@ -1,5 +1,7 @@
 package net.tds.magicpets.item;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -15,5 +17,11 @@ public class ItemEssence extends ItemModjamBase {
     public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
     	
 		return stack;
+    }
+    
+    @SideOnly(Side.CLIENT)
+    public boolean hasEffect(ItemStack par1ItemStack) {
+    	
+        return true;
     }
 }
