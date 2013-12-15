d495fb737b2d12a16b11a930ae52cc9fb4dec06a
Tyler Hancock <darklime@live.ca>
2013-12-15 14:31:17 -0700

    Automated Modjam Commit: 14:31:17.23

--- src/net/tds/magicpets/entity/boss/EntityWaterBoss.java
+++ src/net/tds/magicpets/entity/boss/EntityWaterBoss.java
@@ -1,6 +1,7 @@
 package net.tds.magicpets.entity.boss;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
 import net.minecraft.world.World;
 
 public class EntityWaterBoss extends EntityMagicalBoss {
@@ -23,12 +24,26 @@ public class EntityWaterBoss extends EntityMagicalBoss {
             if (this.getEntityToAttack() != null && this.getEntityToAttack().posY > this.posY) {
             	
                 this.motionY = (0.40000001192092896D - this.motionY) * 0.40000001192092896D;
-                this.moveEntity(this.getEntityToAttack().posX, this.posY, this.getEntityToAttack().posZ);
-                System.out.println(this.getEntityToAttack().getEyeHeight() + " : " + this.getEyeHeight());
             }
     	}
         
     	this.setAir(20);
     	super.onLivingUpdate();
+    }
+    
+    public boolean attackEntityAsMob(Entity entity) {
+    	
+    	if(super.attackEntityAsMob(entity)){
+    		
+    		double rand = Math.random();
+    		
+    		if(rand < 0.13D) {
+    			
+    			entity.motionY = -0.03;
+    			entity.setAir(entity.getAir() - 1);
+    		}
+    	}
+    	
+		return hasAttacked;	
     }
 }
