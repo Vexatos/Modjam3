3fa82201815d2dd843109f4e1654574bd5fb5913
Tyler Hancock <darklime@live.ca>
2013-12-14 19:16:08 -0700

    Automated Modjam Commit: 19:16:08.42

--- src/net/tds/magicpets/event/MobDeathEvent.java
+++ src/net/tds/magicpets/event/MobDeathEvent.java
@@ -1,5 +1,30 @@
 package net.tds.magicpets.event;
 
-public class MobDeathEvent {
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.tds.magicpets.entity.pet.EntityMagicalPet;
 
+public class MobDeathEvent {
+	
+	@ForgeSubscribe
+	public void onMobDeath(LivingDeathEvent event) {
+		
+		if (event.source.getEntity() != null) {
+			
+			Entity source = event.source.getEntity();
+			
+			if (source instanceof EntityPlayer) {
+				
+				EntityPlayer player = (EntityPlayer) source;
+			}
+			
+			if (source instanceof EntityMagicalPet) {
+				
+				EntityMagicalPet pet = (EntityMagicalPet) source;
+			}
+		}
+	}
 }
