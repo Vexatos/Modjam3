fab5bddcc2abc9c652ebeb15fe68388ca9a0fe2d
ShadowChild <shadychild12@gmail.com>
2013-12-15 20:28:12 +0000

    Added entity supprot to elements
    Signed-off-by: ShadowChild <shadychild12@gmail.com>

--- src/net/tds/magicpets/enums/EnumElement.java
+++ src/net/tds/magicpets/enums/EnumElement.java
@@ -1,22 +1,27 @@
 package net.tds.magicpets.enums;
 
+import net.tds.magicpets.entity.passive.*;
+
+
 public enum EnumElement {
 
-    FIRE("fire", 0x680000),
-    WATER("water", 0x0066FF),
-    EARTH("earth", 0x336600),
-    AIR("air", 0x66CCFF),
-    LIGHT("light", 0xFFFF33),
-    UNDEAD("undead", 0x606060); //Iluminati
+    FIRE("fire", 0x680000, EntityBabyFirePet.class),
+    WATER("water", 0x0066FF, EntityBabyWaterPet.class),
+    EARTH("earth", 0x336600, EntityBabyEarthPet.class),
+    AIR("air", 0x66CCFF, EntityBabyAirPet.class),
+    LIGHT("light", 0xFFFF33, EntityBabyLightPet.class),
+    UNDEAD("undead", 0x606060, EntityBabyUndeadPet.class); //Iluminati
 
-    private EnumElement(String type, int colour) {
+    private EnumElement(String type, int colour, Class<? extends EntityMagicalPet> pet) {
 
     	this.type = type;
         this.colour = colour;
+        this.pet = pet;
     }
 
     public String type;
     public int colour;
+    public Class<? extends EntityMagicalPet> pet;
 
     public static EnumElement getType(String type) {
 
